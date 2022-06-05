6ba91f7534682393aeabdd29d6a4fcf51ed52081
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2022-06-05 19:33:03 -0300

    Showing task time in Clock

--- src/components/Timer/Clock/index.tsx
+++ src/components/Timer/Clock/index.tsx
@@ -1,13 +1,22 @@
 import style from "./Clock.module.scss";
 
-export default function Clock() {
+interface Props {
+  time: number | undefined;
+}
+
+export default function Clock({ time = 0 }: Props) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  const [minutesTen, minuteUnit] = String(minutes).padStart(2, "0");
+  const [secondsTen, secondsUnit] = String(seconds).padStart(2, "0");
+
   return (
     <>
-      <span className={style.relogioNumero}>0</span>
-      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>{minutesTen}</span>
+      <span className={style.relogioNumero}>{minuteUnit}</span>
       <span className={style.relogioDivisao}>:</span>
-      <span className={style.relogioNumero}>0</span>
-      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>{secondsTen}</span>
+      <span className={style.relogioNumero}>{secondsUnit}</span>
     </>
   );
 }
--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react";
+import { parseToSeconds } from "../../common/utils/time";
+import { ItemTask } from "../../types/ItemTask";
 import Button from "../Button";
 import Clock from "./Clock";
 import style from "./Timer.module.scss";
 
-export default function Timer() {
+interface Props {
+  selected: ItemTask | undefined;
+}
+
+export default function Timer(props: Props) {
+  const { selected } = props;
+  const [time, setTime] = useState<number>();
+
+  useEffect(() => {
+    if (selected?.time) setTime(parseToSeconds(selected.time));
+  }, [selected]);
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+      <p>Tempo: {time}</p>
       <div className={style.relogioWrapper}>
-        <Clock />
+        <Clock time={time} />
       </div>
       <Button>Começar!</Button>
     </div>
